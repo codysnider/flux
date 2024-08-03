85977b8cbc84734c8cad5311c1220960c9a6f9c1
Cody Snider <cody@codysnider.com>
2024-08-02 20:28:20 -0400

    testing docker build

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 docker build . -t taletelr-flux
 docker run -it --gpus all -p 8501:8501 -e CUDA_VISIBLE_DEVICES=0 -v /data/llama_factory/hf_cache:/root/.cache/huggingface \
-  --memory=24g \
-  --memory-swap=24g \
+  --memory=12g \
+  --memory-swap=12g \
   taletelr-flux
